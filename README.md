3ee5c8a92bccba2a797e91dbdf8d612917114a42
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2021-05-21 00:04:45 +0700

    update

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# shortingurl
-
-Một ứng dụng cho phép rút gọn link đơn giản
-
-#Clone 
-```
-git clone https://github.com/hoangthuc701/shortingurl.git
-```
-
-##Công nghệ sử dụng
-1. Mongoose
-2. Nodejs
-
+# android-r2s hehehe
--- index.js
+++ index.js
@@ -1,37 +1,32 @@
+require('dotenv').config();
 const express = require('express');
-const app= express();
-const BodyParser = require('body-parser');
 const mongoose = require('mongoose');
-var cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
 
+const authRouter = require('./routes/auth');
 
-//connect to db 
-mongoose.connect('mongodb+srv://hoangthuc701:FySN2y5TV4MF2nW@cluster0-fbnn5.mongodb.net/test?retryWrites=true', {useNewUrlParser:true})
-.then(()=>console.log("DB connected."));
-mongoose.connection.on('error',err=>{
-    console.log(err.message);
-})
+const connectDB = async () =>{
+    try {
+        await mongoose.connect('mongodb+srv://tranleanhvu:1234@r2s-database.f9ywc.mongodb.net/r2s-database?retryWrites=true&w=majority', {
+            // Các tham số này là mặc định
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
 
+        console.log("MongoDB Connected")
+    } catch (error) {
+        console.log("eror", error.message);
+        // Restart lại
+        process.exit(1);
+    }
+}
+connectDB();
 
+const app = express();
+app.use(express.json());
 
-//apply midleware
-app.use(express.static(__dirname + '/public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({extended:false}));
-app.use(cors())
-app.use('/',require('./routes/url'));
-app.use(function(req,res,next){
-    res.render('notfound.html');
-})
+app.use('/api/auth', authRouter);
 
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log(`Server is running in port ${PORT}`);
-})
-
+const POST = process.env.POST || 5000;
+app.listen(POST, () => console.log("Server started on port 5000"))
